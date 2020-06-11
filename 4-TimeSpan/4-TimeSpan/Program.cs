e6f03576acd2a5d2400acffd0ec2812b964c44d7
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-11 16:04:22 -0300

    TimeSpan

--- 4-TimeSpan/4-TimeSpan/Program.cs
+++ 4-TimeSpan/4-TimeSpan/Program.cs
@@ -35,6 +35,59 @@ namespace _4_TimeSpan {
             Console.WriteLine(t11);
             Console.WriteLine(t12);
 
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //PROPRIEDADES TIMESPAN:
+
+            TimeSpan tt1 = TimeSpan.MaxValue;
+            TimeSpan tt2 = TimeSpan.MinValue;
+            TimeSpan tt3 = TimeSpan.Zero;
+
+            Console.WriteLine(tt1);
+            Console.WriteLine(tt2);
+            Console.WriteLine(tt3);
+
+            Console.WriteLine("------------------------------------------");
+
+            TimeSpan ttt = new TimeSpan(2, 3, 5, 7, 11);
+
+            Console.WriteLine(ttt);
+
+            Console.WriteLine("1) Days: " + ttt.Days);
+            Console.WriteLine("2) Hours: " + ttt.Hours);
+            Console.WriteLine("3) Minutes: " + ttt.Minutes);
+            Console.WriteLine("4) Seconds: " + ttt.Seconds);
+            Console.WriteLine("5) Milliseconds: " + ttt.Milliseconds);
+            Console.WriteLine("6) Ticks: " + ttt.Ticks);
+
+            Console.WriteLine("7) TotalDays: " + ttt.TotalDays);
+            Console.WriteLine("8) TotalHours: " + ttt.TotalHours);
+            Console.WriteLine("9) TotalMinutes: " + ttt.TotalMinutes);
+            Console.WriteLine("10) TotalSeconds: " + ttt.TotalSeconds);
+            Console.WriteLine("11) TotalMilliseconds: " + ttt.TotalMilliseconds);
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine();
+
+            //OPERAÇÕES COM TIMESPAN:
+
+            TimeSpan ttt1 = new TimeSpan(1, 30, 10);
+            TimeSpan ttt2 = new TimeSpan(0, 10, 5);
+
+            TimeSpan sum = ttt1.Add(ttt2);
+            TimeSpan dif = ttt1.Subtract(ttt2);
+            TimeSpan mult = ttt1.Multiply(2.0);
+            TimeSpan div = ttt1.Divide(2.0);
+
+            Console.WriteLine(sum);
+            Console.WriteLine(dif);
+            Console.WriteLine(mult);
+            Console.WriteLine(div);
 
         }
     }
